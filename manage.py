cff95c32a80ff4aef18b1c6b9dd5694545cb8b86
dizona <dizona@mcmaster.ca>
2023-01-19 00:16:21 -0500

    Updated app.py with initial Connexion setup

--- manage.py
+++ manage.py
@@ -1,6 +1,14 @@
-from app import create_app
+# create_app depreciated with the use of swagger
+# from app import create_app
+import connexion
 
-app = create_app()
+# connecting swagger.yml configuration file with the Flask app
+# specification_dir tells Connexion which directory to look for the config file
+app = connexion.App(__name__, specification_dir="./")
+# Telling app to read the swagger.yml file from specification_dir
+app.add_api("swagger.yml")
+
+# app = create_app()
 
 
 @app.route("/")
