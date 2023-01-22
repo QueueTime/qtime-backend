fdb945ea76cae43bd6eb3e2bd7514d8557a046cd
dizona <dizona@mcmaster.ca>
2023-01-21 22:13:16 -0500

    Removed depreciated functionality

--- app/__init__.py
+++ app/__init__.py
@@ -6,13 +6,3 @@ from firebase_admin import credentials, firestore, initialize_app
 cred = credentials.Certificate("app/key/serviceAccountKey.json")
 default_app = initialize_app(cred)
 firestore_db = firestore.client()
-
-
-# def create_app():
-#     app = Flask(__name__)
-
-#     @app.route("/success")
-#     def health():
-#         return jsonify("success")
-
-#     return app
