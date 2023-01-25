3b7b564396efdff2279be58edc242070d9dbc270
dizona <dizona@mcmaster.ca>
2023-01-25 00:15:26 -0500

    Added suggest new POI function to POI rest handler

--- app/poi_api/poi_rest_handler.py
+++ app/poi_api/poi_rest_handler.py
@@ -2,10 +2,15 @@ from .poi_service import POI_Service
 
 poi_service_instance = POI_Service()
 
-
+# TODO Exception handling
+# TODO type hints
 def get_all_POI():
     return poi_service_instance.get_all_POI()
 
 
 def get_POI(poi_id):
     return poi_service_instance.get_POI(poi_id)
+
+
+def suggest_new_POI():
+    return poi_service_instance.suggest_new_POI()
