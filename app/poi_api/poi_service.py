c5d9c707bfb3fd28c6e129db390e2ef33d203e46
dizona <dizona@mcmaster.ca>
2023-01-25 00:16:02 -0500

    Updated POI get functions

--- app/poi_api/poi_service.py
+++ app/poi_api/poi_service.py
@@ -5,22 +5,30 @@ from .poi_api import POIApi
 
 class POI_Service(POIApi):
     def get_all_POI(self):
-        poi_ref = firestore_db.collection("POI")
-        all_poi = [doc.to_dict() for doc in poi_ref.stream()]
-        return jsonify(all_poi), 200
+        try:
+            poi_ref = firestore_db.collection("POI")
+            all_poi = [doc.to_dict() for doc in poi_ref.stream()]
+            return jsonify(all_poi), 200
+        except Exception as e:
+            return f"An Error Occured: {e}", 404
 
     def get_POI(self, poi_id):
         try:
             poi_ref = firestore_db.collection("POI")
             poi = poi_ref.document(poi_id).get()
-            return jsonify(poi.to_dict()), 200
+            poi_dict = poi.to_dict()
+            if poi_dict is not None:
+                return jsonify(poi.to_dict()), 200
+            else:
+                return f"POI with id: {poi_id}, does not exist", 404
+
         except Exception as e:
             return f"An Error Occured: {e}"
 
     def suggest_new_POI(self):
         pass
 
-    def create_POI_suggestion(self):
+    def save_POI_suggestion(self):
         pass
 
     def fetch_latest_estimated_value(self):
