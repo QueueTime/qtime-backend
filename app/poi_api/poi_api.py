205f24c8371372ee0fc48a8c72c0e1641886b18a
dizona <dizona@mcmaster.ca>
2023-01-29 00:27:01 -0500

    Editing poi abstract class

--- app/poi_api/poi_api.py
+++ app/poi_api/poi_api.py
@@ -1,7 +1,7 @@
 # Common classes and utility methods used by POI API
 
 from abc import ABC, abstractmethod
-from .poi_api import User
+from app import User
 
 
 class POIApi(ABC):
@@ -15,7 +15,8 @@ class POIApi(ABC):
         """Returns the object of a single POI specified"""
         raise NotImplementedError("Base class cannot be used")
 
+    # TODO: Add User to parameter when figured out
     @abstractmethod
-    def suggest_new_POI(self, user: User, suggestion_name, notes):
+    def suggest_new_POI(self):
         """Allows client to suugest a new POI to be added to the POI Collection"""
         raise NotImplementedError("Base class cannot be used")
