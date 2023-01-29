0b39b48e21dff8859f430b3ec83a8761e6fd034b
dizona <dizona@mcmaster.ca>
2023-01-29 00:28:25 -0500

    Added POI suggestion and implemented error handling

--- app/poi_api/poi_rest_handler.py
+++ app/poi_api/poi_rest_handler.py
@@ -1,16 +1,45 @@
 from .poi_service import POI_Service
+from flask import jsonify
+from .poi_errors import POINotFoundError, InvalidPOISuggestionError
 
-poi_service_instance = POI_Service()
+poi_service_class = POI_Service()
 
 # TODO Exception handling
 # TODO type hints
 def get_all_POI():
-    return poi_service_instance.get_all_POI()
+    try:
+        get_all_POI_reponse = poi_service_class.get_all_POI()
+        list_all_poi = get_all_POI_reponse[0]
+        get_all_poi_status = get_all_POI_reponse[1]
+        if get_all_poi_status == 200:
+            return [poi._to_dict() for poi in list_all_poi]
+        else:
+            return jsonify(
+                {
+                    "success": False,
+                    "message": "POI list could not be retrieved.",
+                    "error_code": get_all_poi_status,
+                }
+            )
+    except Exception as e:
+        return jsonify({"success": False, "message": str(e), "error_code": 400})
 
 
 def get_POI(poi_id):
-    return poi_service_instance.get_POI(poi_id)
+    try:
+        get_poi = poi_service_class.get_POI(poi_id)
+        poi = get_poi[0]
+        get_poi_status = get_poi[1]
+        if get_poi_status == 200:
+            return poi._to_dict()
+        else:
+            return jsonify({"success": False, "error_code": get_poi_status})
+    except POINotFoundError as e:
+        return jsonify({"success": False, "message": str(e), "error_code": 404})
 
 
-def suggest_new_POI():
-    return poi_service_instance.suggest_new_POI()
+def suggest_new_POI(poi_suggestion):
+    try:
+        return poi_service_class.suggest_new_POI(poi_suggestion)
+    except Exception as e:
+        return jsonify({"success": False, "message": str(e), "error_code": 400})
--- app/poi_api/poi_service.py
+++ app/poi_api/poi_service.py
@@ -1,6 +1,10 @@
 from app import firestore_db
 from flask import jsonify
 from .poi_api import POIApi
+from .poi_suggestion import POI_suggestions
+from .poi import POI
+from .poi_errors import POINotFoundError, InvalidPOISuggestionError
+from datetime import datetime
 
 
 class POI_Service(POIApi):
@@ -8,9 +12,13 @@ class POI_Service(POIApi):
         try:
             poi_ref = firestore_db.collection("POI")
             all_poi = [doc.to_dict() for doc in poi_ref.stream()]
-            return jsonify(all_poi), 200
+            list_all_poi = list()
+            for poi in all_poi:
+                poi_object = self._create_poi_object(poi)
+                list_all_poi.append(poi_object)
+            return list_all_poi, 200
         except Exception as e:
-            return f"An Error Occured: {e}", 404
+            raise Exception(f"An error occured: {e}")
 
     def get_POI(self, poi_id):
         try:
@@ -18,18 +26,71 @@ class POI_Service(POIApi):
             poi = poi_ref.document(poi_id).get()
             poi_dict = poi.to_dict()
             if poi_dict is not None:
-                return jsonify(poi.to_dict()), 200
+                return self._create_poi_object(poi_dict), 200
             else:
-                return f"POI with id: {poi_id}, does not exist", 404
-
+                raise Exception(f"The POI with the id: {poi_id} could not be found")
         except Exception as e:
-            return f"An Error Occured: {e}"
+            raise POINotFoundError(f"{e}", 404)
 
-    def suggest_new_POI(self):
-        pass
+    def suggest_new_POI(self, poi_suggestion):
+        try:
+            poi_suggestion_ref = firestore_db.collection("POI_proposal").document()
+            pid = poi_suggestion_ref.id
+            poi_suggestion_instance = self._create_POI_suggestion(pid, poi_suggestion)
+            return self._save_POI_suggestion(
+                poi_suggestion_instance, poi_suggestion_ref
+            )
+        except Exception as e:
+            error_code = 400
+            raise InvalidPOISuggestionError(f"Invalid POI submission: {e}", error_code)
 
-    def _save_POI_suggestion(self):
-        pass
+    # TODO: Add User parameter
+    # TODO: Create user object for submitted_by
+    def _create_POI_suggestion(self, pid, poi_suggestion):
+        try:
+            suggestion_name = poi_suggestion.get("suggestion_name")
+            notes = poi_suggestion.get("notes")
+            submission_time = datetime.now()
+            submitted_by = poi_suggestion.get("submitted_by")
+            return POI_suggestions(
+                pid, suggestion_name, notes, submission_time, submitted_by
+            )
+        except Exception as e:
+            raise Exception(f"An error occured: {e}")
+
+    def _save_POI_suggestion(self, poi_suggestion_instance, poi_suggestion_ref) -> str:
+        try:
+            poi_suggestion_dict = poi_suggestion_instance.to_dict()
+            # Retrieves key from push
+            poi_suggestion_post_ref = poi_suggestion_ref.set(
+                poi_suggestion_dict, merge=True
+            )
+            return poi_suggestion_post_ref
+        except Exception as e:
+            raise Exception(f"An error occured: {e}")
+
+    def _create_poi_object(self, poi):
+        try:
+            _id = poi["_id"]
+            name = poi["name"]
+            clasification = poi["class"]
+            hours_of_operation = poi["hours_of_operation"]
+            address = poi["address"]
+            poi_type = poi["type"]
+            location = poi["location"]
+            image_url = poi["image_url"]
+            return POI(
+                _id,
+                name,
+                clasification,
+                hours_of_operation,
+                address,
+                poi_type,
+                location,
+                image_url,
+            )
+        except Exception as e:
+            raise Exception(f"An error occured: {e}")
 
     def _fetch_latest_estimated_value(self):
         pass
