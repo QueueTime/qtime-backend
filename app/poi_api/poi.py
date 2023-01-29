661f2bcedb357c01a01de7bc6fb7e8b036eb07da
dizona <dizona@mcmaster.ca>
2023-01-29 00:26:28 -0500

    Added to_dict function to poi class

--- app/poi_api/poi.py
+++ app/poi_api/poi.py
@@ -3,27 +3,36 @@ from app import firestore_db, common
 from flask import jsonify
 
 
-class POI(common.FirebaseDataEntity):
+class POI:
     def __init__(
         self,
-        db_ref,
+        _id,
         name,
         clasification,
         hours_of_operation,
         address,
-        type,
-        longitude,
-        latitude,
+        poi_type,
+        location,
         image_url,
     ):
-        super().__init__(db_ref)
-        if db_ref is None:
-            self.db_ref = firestore_db
+        self._id = _id
         self.name = name
         self.classification = clasification
         self.hours_of_operation = hours_of_operation
         self.address = address
-        self.type = type
-        self.longitude = longitude
-        self.latitude = latitude
+        self.poi_type = poi_type
+        self.location = location
         self.image_url = image_url
+
+    def _to_dict(self):
+        poi_suggestion_dict = {
+            "_id": self._id,
+            "address": self.address,
+            "class": self.classification,
+            "hours_of_operation": self.hours_of_operation,
+            "image_url": self.image_url,
+            "location": self.location,
+            "name": self.name,
+            "type": self.poi_type,
+        }
+        return poi_suggestion_dict
