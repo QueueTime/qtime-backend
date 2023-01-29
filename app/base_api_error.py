86717e4d308d665c59e6efe2fbbefead2a9a03ee
dizona <dizona@mcmaster.ca>
2023-01-29 00:25:20 -0500

    BaseAPIError class inital commit

---
+++ app/base_api_error.py
@@ -0,0 +1,12 @@
+from flask import jsonify
+
+
+class BaseApiError(Exception):
+    def __init__(self, message, error_code):
+        super().__init__(message)
+        self.error_code = error_code
+
+    def buildError(self):
+        return jsonify(
+            {"success": False, "message": self.message, "error_code": self.error_code}
+        )
