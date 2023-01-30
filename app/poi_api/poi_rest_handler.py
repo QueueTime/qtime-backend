f084eacbed00bf8b07a560d6baa9552956d2aaff
dizona <dizona@mcmaster.ca>
2023-01-30 00:05:06 -0500

    Removed error jsonify

--- app/poi_api/poi_rest_handler.py
+++ app/poi_api/poi_rest_handler.py
@@ -11,7 +11,7 @@ def get_all_POI():
         list_all_poi = poi_service_class.get_all_POI()
         return [poi.to_dict() for poi in list_all_poi], 200
     except Exception as e:
-        return jsonify({"success": False, "message": str(e), "error_code": 400}), 400
+        return {"success": False, "message": str(e), "error_code": 400}, 400
 
 
 def get_POI(poi_id):
@@ -19,9 +19,9 @@ def get_POI(poi_id):
         get_poi = poi_service_class.get_POI(poi_id)
         return get_poi.to_dict(), 200
     except POINotFoundError as e:
-        return jsonify({"success": False, "message": str(e), "error_code": 404}), 404
+        return {"success": False, "message": str(e), "error_code": 404}, 404
     except Exception as e:
-        return jsonify({"success": False, "message": str(e), "error_code": 404}), 404
+        return {"success": False, "message": str(e), "error_code": 404}, 404
 
 
 def suggest_new_POI(poi_suggestion):
@@ -30,4 +30,4 @@ def suggest_new_POI(poi_suggestion):
     except InvalidPOISuggestionError as e:
         return jsonify({"success": False, "message": str(e), "error_code": 400}), 400
     except Exception as e:
-        return jsonify({"success": False, "message": str(e), "error_code": 400}), 400
+        return {"success": False, "message": str(e), "error_code": 400}, 400
