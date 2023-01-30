2995f139812eb26abf43eb3632041307067bb17b
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-01-30 15:49:02 -0500

    Remove init content

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,1 @@
-from flask import Flask, jsonify, request
-from firebase_admin import credentials, firestore, initialize_app
 
-# Initializing Firestore database
-# Can import firestore_db to utilize database
-cred = credentials.Certificate("serviceAccountKey.json")
-default_app = initialize_app(cred)
-firestore_db = firestore.client()
