7a53b945fd7424f73f26e4f0eb1d13073dbb175e
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-01-30 15:48:32 -0500

    Move firebase variables to firebase file

---
+++ app/firebase.py
@@ -0,0 +1,8 @@
+from flask import Flask, jsonify, request
+from firebase_admin import credentials, firestore, initialize_app
+
+# Initializing Firestore database
+# Can import firestore_db to utilize database
+cred = credentials.Certificate("serviceAccountKey.json")
+default_app = initialize_app(cred)
+firestore_db = firestore.client()
