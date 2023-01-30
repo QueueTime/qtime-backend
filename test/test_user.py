b95aa26908a4be3f45fd27bcf751f6957381d14b
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-01-30 15:36:22 -0500

    Fix formatting with black

--- test/test_user.py
+++ test/test_user.py
@@ -4,14 +4,14 @@ from app.user_api.User import User
 from app.user_api.errors import UserNotFoundError
 import json
 
-class TestUser(unittest.TestCase):
 
+class TestUser(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         cred = credentials.Certificate("./serviceAccountKey.json")
         self.firestore_db = firestore.client()
-        self.users_ref = self.firestore_db.collection('users')
-    
+        self.users_ref = self.firestore_db.collection("users")
+
     def test_user_push_and_fetch(self):
         user = User(self.users_ref, "test@test.com", "", 0, False, "", 0, 0, {}, False)
         user.push()
@@ -19,8 +19,9 @@ class TestUser(unittest.TestCase):
         self.assertEqual(user.to_json(), fetched_user.to_json())
 
     def test_error_handling(self):
-        self.assertRaises(UserNotFoundError, User.get, self.users_ref, "test@nonexistent.com")
+        self.assertRaises(
+            UserNotFoundError, User.get, self.users_ref, "test@nonexistent.com"
+        )
 
     def tearDown(self):
-        self.users_ref.document('test@test.com').delete()
-
+        self.users_ref.document("test@test.com").delete()
