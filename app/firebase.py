f2490f16cb50c44cd36d4ef1678ccb7dfd73dc3b
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-02-04 13:39:25 -0500

    Add event collection name string

--- app/firebase.py
+++ app/firebase.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from firebase_admin import credentials, firestore, initialize_app
 
+EVENTS_COLLECTION = "events"
+
 # Initializing Firestore database
 # Can import firestore_db to utilize database
 cred = credentials.Certificate("serviceAccountKey.json")
