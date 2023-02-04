374a01421020accf07057e9ec11b917e30f69fb1
dizona <dizona@mcmaster.ca>
2023-02-04 16:34:03 -0500

    Updated POI class to implment FireBaseEntity

--- app/poi_api/poi.py
+++ app/poi_api/poi.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 from app import common
 from app.firebase import firestore_db
+from app.poi_api.poi_errors import POINotFoundError
 from flask import jsonify
 
 
-class POI:
+class POI(common.FirebaseDataEntity):
     def __init__(
         self,
+        db_ref,
         _id,
         name,
         clasification,
@@ -16,6 +18,7 @@ class POI:
         location,
         image_url,
     ):
+        super().__init__(db_ref)
         self._id = _id
         self.name = name
         self.classification = clasification
@@ -25,8 +28,30 @@ class POI:
         self.location = location
         self.image_url = image_url
 
+    def get(db_ref, id):
+        target_data = db_ref.document(id).get()
+        if not target_data.exists:
+            raise POINotFoundError(id)
+        return POI.from_dict(db_ref, target_data.to_dict())
+
+    def from_dict(db_ref, dict):
+        try:
+            return POI(
+                db_ref,
+                dict["_id"],
+                dict["address"],
+                dict["class"],
+                dict["hours_of_operation"],
+                dict["image_url"],
+                dict["location"],
+                dict["name"],
+                dict["type"],
+            )
+        except KeyError as e:
+            raise common.BadDataError("Missing data from poi data: " + str(e))
+
     def to_dict(self):
-        poi_suggestion_dict = {
+        poi_dict = {
             "_id": self._id,
             "address": self.address,
             "class": self.classification,
@@ -36,4 +61,11 @@ class POI:
             "name": self.name,
             "type": self.poi_type,
         }
-        return poi_suggestion_dict
+        return poi_dict
+
+    def push(self, merge=True):
+        target_ref = self.db_reference.document(self._id)
+        target_ref.set(self.to_dict(), merge=merge)
+
+    def __eq__(self, other):
+        return self._id == other._id
