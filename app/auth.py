9237945013ab255b4878cf65bef6e176f42f9f57
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-02-05 16:28:39 -0500

    Add auth decorator

---
+++ app/auth.py
@@ -0,0 +1,60 @@
+from firebase_admin import auth
+from firebase_admin._user_mgt import UserRecord
+from werkzeug.exceptions import Unauthorized
+from typing import Dict, Any
+from functools import wraps
+
+
+def with_auth_user(func):
+    """
+    Decorator to transform attach the firebase user record to the function
+    using the token_info decoded from the Authorization header.
+
+    Usage
+    ```
+    @with_auth_user
+    def my_function(user: UserRecord, **kwargs):
+        ...
+    ```
+    Note: It's important to have **kwargs to pass `token_info` arg to the decorator.
+
+    :param func: Function to decorate
+    :return: Decorated function
+    """
+
+    @wraps(func)
+    def wrapper(*args, token_info: Dict[str, Any], **kwargs):
+        try:
+            user: UserRecord = auth.get_user(token_info["uid"])
+            kwargs["user"] = user
+        except ValueError as e:
+            return {"error": "Invalid user ID", "message": str(e)}, 400
+        except auth.UserNotFoundError as e:
+            return {"error": "User not found", "message": str(e)}, 404
+        except Exception as e:
+            return {"error": "Unknown error", "message": str(e)}, 500
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def validate_token(token: str) -> Dict[str, Any]:
+    """
+    Validate and decode a JWT token using Firebase
+
+    :param token: JWT token passed as a Bearer token in the Authorization header
+    :return: Dictionary of value pairs from the decoded JWT
+
+    :raises Unauthorized: If the token is invalid, expired, revoked, or the certificate cannot be fetched
+    :raises ValueError: If the token is not a string or empty
+    """
+    try:
+        return auth.verify_id_token(token)
+    except (
+        auth.InvalidIdTokenError,
+        auth.ExpiredIdTokenError,
+        auth.RevokedIdTokenError,
+        auth.CertificateFetchError,
+    ) as e:
+        raise Unauthorized(f"Invalid token. {str(e)}") from e
