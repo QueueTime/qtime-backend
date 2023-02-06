fa9a6c14a142d6aa13151c91371eb77130390d85
dizona <dizona@mcmaster.ca>
2023-02-05 22:14:10 -0500

    Updated error codes in error class

--- app/poi_api/poi_errors.py
+++ app/poi_api/poi_errors.py
@@ -2,10 +2,10 @@ from app.base_api_error import BaseApiError
 
 
 class POINotFoundError(BaseApiError):
-    def __init__(self, message, error_code):
+    def __init__(self, message, error_code=404):
         super().__init__(message, error_code)
 
 
 class InvalidPOISuggestionError(BaseApiError):
-    def __init__(self, message, error_code):
+    def __init__(self, message, error_code=400):
         super().__init__(message, error_code)
