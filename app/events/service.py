a433170cad9158bf9fdd3f400444e3dce766fda5
Ben Kostiuk <benkostiuk1@gmail.com>
2023-02-06 16:32:53 -0500

    [FEAT] Add events to QTime backend + typechecking (#23)
    * Create events package
    
    * Create event object
    
    * Add simple map for easy conversions
    
    * Add mypy to lint workflow
    
    * Update deps with mypy
    
    * Add mypy config file
    
    * Fix lint file fomatting
    
    * Fix bug for non-existent payload
    
    * Add extra referral category
    
    * Remove line from __init__
    
    * Add event collection name string
    
    * Add service methods for events
    
    * Fix styling of lint workflow
    
    * Add mypy to Readme
    
    * Remvoe static methods
    
    * Add check for referral event if users are the same
    
    * Add check for referral event if users are the same
    
    * Add tests for the event service
    
    * Fix lint file to also checkout code for mypy

---
+++ app/events/service.py
@@ -0,0 +1,122 @@
+from app.firebase import firestore_db, EVENTS_COLLECTION
+from app.user_api.user_api import User
+from .event import (
+    Event,
+    EventType,
+    WaitTimeSubmitPayload,
+    WaitTimeConfirmPayload,
+    RewardPointsAddPayload,
+    RewardSource,
+)
+from app.poi_api.poi import POI
+
+
+def generate_account_signup_event(user: User):
+    """
+    Generate an event for when a user signs up for the first time.
+
+    :param user: The user who signed up
+    """
+    _save_to_events_collection(Event(EventType.ACCOUNT_SIGNUP, user.email))
+
+
+def generate_account_delete_event(user: User):
+    """
+    Generate an event for when a user deletes their account.
+
+    :param user: The user who deleted their account
+    """
+    _save_to_events_collection(Event(EventType.ACCOUNT_DELETION, user.email))
+
+
+def generate_waittime_submit_event(
+    user: User, poi: POI, estimate_submitted: int, points_awarded: int
+):
+    """
+    Generate an event for when a user submits a wait time estimate.
+
+    :param user: User who submitted the wait time estimate
+    :param poi: POI that the user submitted the wait time estimate for
+    :param estimate_submitted: Estimate submitted by the user
+    :param points_awarded: Number of points awarded to the user for submitting the wait time estimate
+    """
+    _save_to_events_collection(
+        Event(
+            EventType.WAITTIME_SUBMIT,
+            user.email,
+            payload=WaitTimeSubmitPayload(estimate_submitted, poi._id),
+        )
+    )
+    _generate_reward_point_change_event(
+        user.email, RewardSource.WAITTIME_SUBMIT, points_awarded
+    )
+
+
+def generate_waittime_confirm_event(user: User, poi: POI, points_awarded: int):
+    """
+    Generate an event for when a user confirms a wait time estimate.
+
+    :param user: User who confirmed the wait time estimate
+    :param poi: POI that the user confirmed the wait time estimate for
+    :param points_awarded: Number of points awarded to the user for confirming the wait time estimate
+    """
+    _save_to_events_collection(
+        Event(
+            EventType.WAITTIME_CONFIRM,
+            user.email,
+            payload=WaitTimeConfirmPayload(poi._id),
+        )
+    )
+    _generate_reward_point_change_event(
+        user.email, RewardSource.WAITTIME_CONFIRM, points_awarded
+    )
+
+
+def generate_referral_event(
+    user: User, user_with_referral_code: User, points_awarded: int
+):
+    """
+    Generate an event when a user signs up using a referral code.
+
+    :param user: User who signed up using a referral code
+    :param user_with_referral_code: User who's referral code was used
+    :param points_awarded: Number of points awarded to both users
+    :throws ValueError: If the user who signed up and the user who's referral code was used are the same
+    """
+    if user.email == user_with_referral_code.email:
+        raise ValueError("User cannot refer themselves")
+
+    _generate_reward_point_change_event(
+        user.email, RewardSource.REFERRED_BONUS, points_awarded
+    )
+    _generate_reward_point_change_event(
+        user_with_referral_code.email, RewardSource.REFERRAL_BONUS, points_awarded
+    )
+
+
+def _generate_reward_point_change_event(
+    email: str, source: RewardSource, points_change: int
+):
+    """
+    Generate a general point change event.
+
+    :param email: Email of the user who's points are being added
+    :param source: Source of the reward points being added
+    :param points_change: Number of points being added or subtracted
+    """
+    _save_to_events_collection(
+        Event(
+            EventType.REWARD_POINTS_ADD,
+            email,
+            payload=RewardPointsAddPayload(source, points_change),
+        )
+    )
+
+
+def _save_to_events_collection(event: Event):
+    """
+    Save an event to the events collection in Firestore.
+
+    :param event: Event to save
+    """
+    firestore_db.collection(EVENTS_COLLECTION).add(event.to_dict())
---
+++ test/events/test_service.py
@@ -0,0 +1,140 @@
+import unittest
+from unittest.mock import patch, Mock, MagicMock, ANY, call
+
+from app.events import service as event_service
+from app.user_api.user_api import User
+from app.poi_api.poi import POI
+
+
+@patch("app.events.service.firestore_db")
+class TestEventService(unittest.TestCase):
+    """Test the event service"""
+
+    @classmethod
+    def setUpClass(self):
+        self.email = "test@sample.ca"
+        self.poi_id = "SAMPLE_POI"
+        self.points_awarded = 25
+        self.user = User(None, self.email, "XJFEKDG", 0)
+        self.poi = POI(
+            self.poi_id, "Testing poi", "Testing", None, None, None, None, None
+        )
+
+    def test_generate_account_signup_event(self, firebase_mock):
+        event_service.generate_account_signup_event(self.user)
+        firebase_mock.collection().add.assert_called_once_with(
+            {
+                "type": "account_signup_event",
+                "user": self.email,
+                "payload": None,
+                "created": ANY,
+            }
+        )
+
+    def test_generate_account_delete_event(self, firebase_mock):
+        event_service.generate_account_delete_event(self.user)
+        firebase_mock.collection().add.assert_called_once_with(
+            {
+                "type": "account_delete_event",
+                "user": self.email,
+                "payload": None,
+                "created": ANY,
+            }
+        )
+
+    def test_generate_waittime_submit_event(self, firebase_mock):
+        event_service.generate_waittime_submit_event(
+            self.user, self.poi, 50, self.points_awarded
+        )
+        calls = [
+            call(
+                {
+                    "type": "waittime_submit_event",
+                    "user": self.email,
+                    "payload": {
+                        "poi_id": self.poi_id,
+                        "estimate_submitted": 50,
+                    },
+                    "created": ANY,
+                }
+            ),
+            call(
+                {
+                    "type": "reward_points_add_event",
+                    "user": self.email,
+                    "payload": {
+                        "source": "waittime_submit",
+                        "points_change": self.points_awarded,
+                    },
+                    "created": ANY,
+                }
+            ),
+        ]
+        firebase_mock.collection().add.assert_has_calls(calls, any_order=True)
+
+    def test_generate_waittime_confirm_event(self, firebase_mock):
+        event_service.generate_waittime_confirm_event(
+            self.user, self.poi, self.points_awarded
+        )
+        calls = [
+            call(
+                {
+                    "type": "waittime_confirm_event",
+                    "user": self.email,
+                    "payload": {"poi_id": self.poi_id},
+                    "created": ANY,
+                }
+            ),
+            call(
+                {
+                    "type": "reward_points_add_event",
+                    "user": self.email,
+                    "payload": {
+                        "source": "waittime_confirm",
+                        "points_change": self.points_awarded,
+                    },
+                    "created": ANY,
+                }
+            ),
+        ]
+        firebase_mock.collection().add.assert_has_calls(calls, any_order=True)
+
+    def test_generate_referral_event_same_user(self, firebase_mock):
+        with self.assertRaises(ValueError):
+            event_service.generate_referral_event(
+                self.user, self.user, self.points_awarded
+            )
+
+    def test_generate_referral_event(self, firebase_mock):
+        referral_email = "refferer@sample.com"
+        same_referral_user = User(None, referral_email, "ASDKGUE")
+
+        event_service.generate_referral_event(
+            self.user, same_referral_user, self.points_awarded
+        )
+
+        calls = [
+            call(
+                {
+                    "type": "reward_points_add_event",
+                    "user": self.email,
+                    "payload": {
+                        "source": "referred_bonus",
+                        "points_change": self.points_awarded,
+                    },
+                    "created": ANY,
+                }
+            ),
+            call(
+                {
+                    "type": "reward_points_add_event",
+                    "user": referral_email,
+                    "payload": {
+                        "source": "referral_bonus",
+                        "points_change": self.points_awarded,
+                    },
+                    "created": ANY,
+                }
+            ),
+        ]
+        firebase_mock.collection().add.assert_has_calls(calls, any_order=True)
