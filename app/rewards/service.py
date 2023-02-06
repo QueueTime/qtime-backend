f89872f5a57507a04e4e55f5cd970d48683d4e0f
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-02-06 17:10:55 -0500

    Create service for rewards

---
+++ app/rewards/service.py
@@ -0,0 +1,40 @@
+import random
+
+from app.firebase import firestore_db, REFERRAL_CODES_COLLECTION
+
+
+def create_unique_referral_code() -> str:
+    """
+    Create a unique 6 character referral code.
+
+    :return: A unique 6 character referral code.
+    """
+    CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    code = _generate_unique_code(CHARS, 6)
+    while _referral_code_exists(code):
+        code = _generate_unique_code(CHARS, 6)
+    return code
+
+
+def _generate_unique_code(CHARS: str, length: int) -> str:
+    """
+    Generate a unique code from a set of characters.
+
+    :param CHARS: String with the characters to generate the code from.
+    :param length: The length of the code to generate.
+    :return: A unique code of given length.
+    """
+    return "".join(random.choices(CHARS, k=length))
+
+
+def _referral_code_exists(code: str) -> bool:
+    """
+    Check if a referral code exists in the database.
+
+    :param code: The referral code to check.
+    :return: True if the referral code exists, False otherwise.
+    """
+
+    return (
+        firestore_db.collection(REFERRAL_CODES_COLLECTION).document(code).get().exists
+    )
