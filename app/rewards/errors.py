44ece3881313bae017a5c65eaaf1e90f2a1801b1
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-02-06 16:49:39 -0500

    Add errors for rewards api

--- app/rewards/errors.py
+++ app/rewards/errors.py
@@ -1,17 +1,21 @@
+from typing import Tuple, Dict
+
 from app.base_api_error import BaseApiError
 
 
 class ReferralCodeNotFound(BaseApiError):
-    def __init__(self, message):
+    def __init__(self, message: str):
         super(message, 404)
+        self.message = message
 
-    def jsonify(self):
+    def jsonify(self) -> Tuple[Dict[str, str], int]:
         return {"error": "Referral code not found", "message": self.message}, 404
 
 
 class InvalidReferralOperation(BaseApiError):
-    def __init__(self, message):
+    def __init__(self, message: str):
         super(message, 400)
+        self.message = message
 
-    def jsonify(self):
+    def jsonify(self) -> Tuple[Dict[str, str], int]:
         return {"error": "Invalid referral operation", "message": self.message}, 400
