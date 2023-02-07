8cdb0a41e8aeca10ace941e9d3582c09ab3c7d09
Youssef Mikhail <youssef.mikhail@outlook.com>
2023-02-07 13:59:35 -0500

    Update user tests to mock firebase calls

--- test/test_user.py
+++ test/test_user.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch, Mock, MagicMock
 from firebase_admin import firestore
 from app import firebase
 from app.user_api.User import User
@@ -11,48 +12,79 @@ import json
 class TestUser(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.firestore_db = firestore.client()
-        self.users_ref = self.firestore_db.collection("users")
-
-    def test_user_push_and_fetch(self):
-        user = User(self.users_ref, "test@test.com", "", 0, False, "", 0, 0, {}, False)
-        user.push()
-        fetched_user = User.get(self.users_ref, "test@test.com")
-        self.assertEqual(user.to_json(), fetched_user.to_json())
-
-    def test_error_handling(self):
-        self.assertRaises(
-            UserNotFoundError, User.get, self.users_ref, "test@nonexistent.com"
+        self.sample_user_dict = {
+            "email": "test@test.com",
+            "referral_code": "",
+            "reward_point_balance": 0,
+            "notification_setting": False,
+            "time_in_line": 0,
+            "num_lines_participated": 0,
+            "poi_frequency": {},
+            "hasCompletedOnboarding": False,
+        }
+        self.sample_user = User(
+            email="test@test.com",
+            referral_code="",
+            reward_point_balance=0,
+            notification_setting=False,
+            time_in_line=0,
+            num_lines_participated=0,
+            poi_frequency={},
+            hasCompletedOnboarding=False,
         )
 
-    def tearDown(self):
-        self.users_ref.document("test@test.com").delete()
+    def test_to_from_dict(self):
+        self.assertEqual(
+            self.sample_user.to_json(), User.from_dict(self.sample_user_dict).to_json()
+        )
 
 
+@patch("app.user_api.user_service.users_collection")
 class TestUserService(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.firestore_db = firestore.client()
-        self.users_ref = self.firestore_db.collection("users")
         self.user_service = user_service.User_Service()
-        self.test_user = User(
-            self.users_ref, "test@test.com", "", 0, False, "", 0, 0, {}, False
+        self.sample_user_dict = {
+            "email": "test@test.com",
+            "referral_code": "",
+            "reward_point_balance": 0,
+            "notification_setting": False,
+            "time_in_line": 0,
+            "num_lines_participated": 0,
+            "poi_frequency": {},
+            "hasCompletedOnboarding": False,
+        }
+        self.sample_user = User(
+            email="test@test.com",
+            referral_code="",
+            reward_point_balance=0,
+            notification_setting=False,
+            time_in_line=0,
+            num_lines_participated=0,
+            poi_frequency={},
+            hasCompletedOnboarding=False,
         )
-        self.test_user.push()
 
-    def test_find_user(self):
-        self.assertEqual(self.test_user, self.user_service.findUser("test@test.com"))
+    def test_find_user(self, user_collection_mock):
+        user_collection_mock.document().get().to_dict = MagicMock(
+            return_value=self.sample_user_dict
+        )
+        self.assertEqual(self.sample_user, self.user_service.findUser("test@test.com"))
+        user_collection_mock.document().get().exists = False
         self.assertRaises(
             UserNotFoundError, self.user_service.findUser, "test@nonexistent.com"
         )
 
-    def test_delete_user(self):
+    def test_delete_user(self, user_collection_mock):
+        self.user_service.deleteUser(self.sample_user)
+        user_collection_mock.document().delete.assert_called_once()
+        user_collection_mock.document().get().exists = False
         self.assertRaises(
             UserNotFoundError,
             self.user_service.deleteUser,
-            User(self.users_ref, "test@nonexistent.com"),
+            User("test@nonexistent.com"),
         )
-        self.user_service.deleteUser(self.test_user)
-        self.assertRaises(UserNotFoundError, User.get, self.users_ref, "test@test.com")
-        # cleanup
-        self.test_user.push()
+
+    def test_update_user(self, user_collection_mock):
+        self.user_service.updateUser(self.sample_user)
+        user_collection_mock.document().set.assert_called_once()
