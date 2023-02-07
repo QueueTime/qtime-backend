a6e3eb40a09defe6d1a9beaf0029fb24400cf034
Youssef Mikhail <youssef.mikhail@outlook.com>
2023-02-07 14:01:36 -0500

    Add user collection name to firebase.py

--- app/firebase.py
+++ app/firebase.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from firebase_admin import credentials, firestore, initialize_app
 
 EVENTS_COLLECTION = "events"
+USERS_COLLECTION = "users"
 
 # Initializing Firestore database
 # Can import firestore_db to utilize database
