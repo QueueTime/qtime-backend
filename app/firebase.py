a96ea9ef1d92fcdb3ca35799f74051a4bb6cadf3
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-02-08 17:18:00 -0500

    Move firebase setup not needed for tests to manage.py

--- app/firebase.py
+++ app/firebase.py
@@ -1,12 +1,11 @@
-from flask import Flask, jsonify, request
-from firebase_admin import credentials, firestore, initialize_app
+from firebase_admin import firestore
 
 EVENTS_COLLECTION = "events"
 USERS_COLLECTION = "users"
 REFERRAL_CODES_COLLECTION = "referral_codes"
 
-# Initializing Firestore database
-# Can import firestore_db to utilize database
-cred = credentials.Certificate("serviceAccountKey.json")
-default_app = initialize_app(cred)
+# # Initializing Firestore database
+# # Can import firestore_db to utilize database
+# cred = credentials.Certificate("serviceAccountKey.json")
+# default_app = initialize_app(cred)
 firestore_db = firestore.client()
--- manage.py
+++ manage.py
@@ -1,6 +1,15 @@
 import connexion
 from utils import combine_specifications
+from firebase_admin import credentials, initialize_app
 
+
+# Initializing Firestore database
+# Can import firestore_db to utilize database
+def initialize_firebase():
+    initialize_app(credentials.Certificate("serviceAccountKey.json"))
+
+
+initialize_firebase()
 app = connexion.App(__name__)
 # Tell app to read the combined openapi specification
 app.add_api(
