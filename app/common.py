86f6d26cd4955cd8e6ab62823e749f12f273b011
Youssef Mikhail <youssef.mikhail@outlook.com>
2023-02-08 15:28:10 -0500

    Remove FirebaseDataEntity

--- app/common.py
+++ app/common.py
@@ -2,45 +2,10 @@
 # Common classes and utility methods used throughout the entire application
 ###
 
-from abc import ABC, abstractmethod, abstractclassmethod
 import json
 from typing import Dict, Any
 
 from app.base_api_error import BaseApiError
-
-
-class FirebaseDataEntity(ABC):
-    """
-    Base class for data classes to be used wth Firestore.
-    """
-
-    def __init__(self):
-        """
-        Initialize new data instance. Additional parameters should be added to child classes
-        for any needed fields.
-        """
-
-    @abstractclassmethod
-    def from_dict(dict):
-        """Class function that creates a new data class from a Python dictionary"""
-        raise NotImplementedError("Base class cannot be used")
-
-    @abstractmethod
-    def to_dict(self):
-        """Return all properties in a Python dict"""
-        raise NotImplementedError("Base class cannot be used")
-
-    @abstractmethod
-    def __eq__(self, other):
-        """Compares equality by looking at primary keys"""
-        raise NotImplementedError("Base class cannot be used")
-
-
-class BadDataError(BaseApiError):
-    """Used when receiving unexpected data from Firebase or clients"""
-
-    def __init__(self, message):
-        super().__init__(message, 400)
 
 
 class SimpleMap:
