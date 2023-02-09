97ae8aa9459135ae21288128c803042190a07fe9
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-02-08 21:14:17 -0500

    Remove comments

--- app/firebase.py
+++ app/firebase.py
@@ -4,8 +4,4 @@ EVENTS_COLLECTION = "events"
 USERS_COLLECTION = "users"
 REFERRAL_CODES_COLLECTION = "referral_codes"
 
-# # Initializing Firestore database
-# # Can import firestore_db to utilize database
-# cred = credentials.Certificate("serviceAccountKey.json")
-# default_app = initialize_app(cred)
 firestore_db = firestore.client()
