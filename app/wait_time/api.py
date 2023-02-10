75af33dbafad113567de5588f04f6dc47bac8225
Youssef Mikhail <youssef.mikhail@outlook.com>
2023-02-10 16:52:38 -0500

    Add typing to wait_time.api

--- app/wait_time/api.py
+++ app/wait_time/api.py
@@ -1,14 +1,18 @@
 from flask import jsonify
 from app.auth import with_auth_user
+from app.user.user import User
+from typing import Dict, Any
 
 
 @with_auth_user
-def update_user_location(user, location_data, **kwargs):
+def update_user_location(user: User, location_data: Dict[str, float], **kwargs):
     # TODO: implement uploading user location
     return jsonify(location_data), 200
 
 
 @with_auth_user
-def submit_user_estimate(user, estimate_data, poi_id, **kwargs):
+def submit_user_estimate(
+    user: User, estimate_data: Dict[str, int], poi_id: str, **kwargs
+):
     # TODO: implement user estimate submission
     return jsonify(estimate_data), 200
