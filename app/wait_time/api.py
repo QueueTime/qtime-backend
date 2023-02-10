ddc6cd8a29074052bbcffc61adf0458c75ef41cc
Youssef Mikhail <youssef.mikhail@outlook.com>
2023-02-10 15:05:10 -0500

    Update wait time API spec

--- app/wait_time/api.py
+++ app/wait_time/api.py
@@ -6,3 +6,9 @@ from app.auth import with_auth_user
 def update_user_location(user, location_data, **kwargs):
     # TODO: implement uploading user location
     return jsonify(location_data), 200
+
+
+@with_auth_user
+def submit_user_estimate(user, estimate_data, poi_id, **kwargs):
+    # TODO: implement user estimate submission
+    return jsonify(estimate_data), 200
