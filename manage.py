4f00a9224eb6cd2c3a5d4437d7bca700f03ca962
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-02-10 21:58:03 -0500

    Refactor manage.py to be testable

--- manage.py
+++ manage.py
@@ -9,17 +9,22 @@ def initialize_firebase():
     initialize_app(credentials.Certificate("serviceAccountKey.json"))
 
 
-initialize_firebase()
-app = connexion.App(__name__)
-# Tell app to read the combined openapi specification
-app.add_api(
-    combine_specifications(
-        "./base_swagger.yaml",
-        "./app/poi_api/spec.yaml",
-        "./app/user/spec.yaml",
-        "./app/rewards/spec.yaml",
+def start_server(name: str):
+    app = connexion.App(name)
+    # Tell app to read the combined openapi specification
+    app.add_api(
+        combine_specifications(
+            "./base_swagger.yaml",
+            "./app/poi_api/spec.yaml",
+            "./app/user/spec.yaml",
+            "./app/rewards/spec.yaml",
+        )
     )
-)
+    return app
+
+
+initialize_firebase()
+app = start_server(__name__)
 
 
 @app.route("/")
