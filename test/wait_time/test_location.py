43dce8766146e47ec67c41e147b31ed4f67861c0
Youssef Mikhail <youssef.mikhail@outlook.com>
2023-02-11 17:42:03 -0500

    Add tests for location data class

---
+++ test/wait_time/test_location.py
@@ -0,0 +1,24 @@
+import unittest
+from app.wait_time.location import Location
+from datetime import datetime
+
+
+class TestLocation(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        current_datetime = datetime.now()
+        self.sample_location = Location(
+            "abcd1234", 43.263532187492686, -79.91758503073444, current_datetime
+        )
+        self.sample_dict = {
+            "aid": "abcd1234",
+            "latitude": 43.263532187492686,
+            "longitude": -79.91758503073444,
+            "timestamp": current_datetime,
+        }
+
+    def test_to_from_dict(self):
+        self.assertEqual(
+            self.sample_location.to_json(),
+            Location.from_dict("abcd1234", self.sample_dict).to_json(),
+        )
