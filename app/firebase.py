8234d8ef25b54ce12440fa5fac4f205815ee6faa
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-02-12 14:46:52 -0500

    Fix firebase client to be a function

--- app/firebase.py
+++ app/firebase.py
@@ -4,4 +4,6 @@ EVENTS_COLLECTION = "events"
 USERS_COLLECTION = "users"
 REFERRAL_CODES_COLLECTION = "referral_codes"
 
-firestore_db = firestore.client()
+
+def firestore_db():
+    return firestore.client()
