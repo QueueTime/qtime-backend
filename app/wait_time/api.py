658102a2a6cacc3f53acf48dd9b9dca768d42b7c
Youssef Mikhail <youssef.mikhail@outlook.com>
2023-02-14 15:39:14 -0500

    Implement more endpoints

--- app/wait_time/api.py
+++ app/wait_time/api.py
@@ -2,12 +2,23 @@ from flask import jsonify
 from app.auth import with_auth_user
 from app.user.user import User
 from typing import Dict, Any
+from .location import Location
+from .service import uid_to_aid, update_location, add_wait_time_suggestion
+from app.common import BaseApiError
 
 
 @with_auth_user
-def update_user_location(user: User, location_data: Dict[str, float], **kwargs):
-    # TODO: implement uploading user location
-    return jsonify(location_data), 200
+def update_user_location(
+    user: User, location_data: Dict[str, float], token_info: Dict[str, Any]
+):
+    try:
+        user_location = Location.from_dict(uid_to_aid(token_info["uid"]), location_data)
+        update_location(user_location)
+    except BaseApiError as e:
+        return e.buildError()
+    except Exception as e:
+        return {"error": str(e)}, 500
+    return None, 204
 
 
 @with_auth_user
@@ -15,4 +26,13 @@ def submit_user_estimate(
     user: User, estimate_data: Dict[str, int], poi_id: str, **kwargs
 ):
     # TODO: implement user estimate submission
-    return jsonify(estimate_data), 200
+    # If we need more POI data, change this to fetch the actual POI object instead of just using poi_id
+    if estimate_data["wait_time_estimate"] < 0:
+        return {"error": "Invalid wait time submission"}, 400
+    try:
+        add_wait_time_suggestion(user, poi_id, estimate_data["wait_time_estimate"])
+    except BaseApiError as e:
+        return e.buildError()
+    except Exception as e:
+        return {"error", str(e)}, 500
+    return None, 204
--- app/wait_time/service.py
+++ app/wait_time/service.py
@@ -1,5 +1,46 @@
 # Service class for the Wait Time API
 
 from app.firebase import firestore_db, POI_QUEUE_COLLECTION
+from app.user.user import User
+from app.wait_time.location import Location
+from app.events.service import generate_waittime_submit_event
+from app.locations.poi.service import get_details_for_POI
+from app.rewards.reward_values import POINTS_FOR_TIME_SUBMISSION
 
-poi_queue_collection = firestore_db.collection(POI_QUEUE_COLLECTION)
+
+def poi_queue_collection():
+    return firestore_db().collection(POI_QUEUE_COLLECTION)
+
+
+def uid_to_aid(uid: str) -> str:
+    """
+    Convert a user UID to its associated Anonymous ID (AID)
+
+    :param uid: User's UID
+    :returns: string containing user's AID
+    """
+    # TODO: Implement hashing of UID to generate AID
+    # Temporarily just return UID as AID
+    return uid
+
+
+def update_location(location: Location):
+    """
+    Update location on Firestore database
+
+    :param location: Location data to upload
+    """
+
+    poi_queue_collection().document(location.aid).set(location.to_dict())
+
+
+def add_wait_time_suggestion(user: User, poi_id: str, time_estimate: int):
+    """
+    Add a new wait time suggestion to Firestore
+    """
+    # TODO: Computation needs to be added here. For now it just creates an event
+    # TODO: Should be a generate_waittime_confirm_event if the wait time suggestion matches current wait time
+
+    generate_waittime_submit_event(
+        user, get_details_for_POI(poi_id), time_estimate, POINTS_FOR_TIME_SUBMISSION
+    )
