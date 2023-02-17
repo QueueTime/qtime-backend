2b3bc1bd23a62d776a83407df101c163462593ba
dizona <dizona@mcmaster.ca>
2023-02-17 00:03:34 -0500

    Temporarily commented out test case

--- test/poi/test_poi_service.py
+++ test/poi/test_poi_service.py
@@ -1,48 +1,45 @@
 # import unittest
-# from app.locations.service import POI_Service
+# from unittest.mock import patch, ANY, call, MagicMock
+# from app.locations.service import list_POI
 # from app.locations.poi_suggestion import POI_suggestion
 # from app.locations.errors import POINotFoundError, InvalidPOISuggestionError
-# from firebase_admin import credentials, initialize_app, firestore
+# from app.user.api import User
+# from app.locations.poi import POI
 
 
-# class TestUser(unittest.TestCase):
+# @patch("app.locations.service.firestore_db")
+# class TestPOIService(unittest.TestCase):
 #     @classmethod
 #     def setUpClass(self):
-#         cred = credentials.Certificate("app/key/serviceAccountKey.json")
-#         self.firestore_db = firestore.client()
-#         self.poi_ref = self.firestore_db.collection("POI")
-#         self.poi_sug_ref = self.firestore_db.collection("POI_proposal")
-#         self.poi_service = POI_Service()
-#         self.poi_suggestion = {
-#             "suggestion_name": "TEST",
-#             "notes": "Test case",
-#             "submitted_by": "P-body and Atlas",
+#         self.email = "test@sample.ca"
+#         self.poi_id = "SAMPLE_POI"
+#         self.points_awarded = 25
+#         self.user = User(self.email, "XJFEKDG", 0)
+#         self.poi = POI(
+#             self.poi_id, "Testing poi", "Testing", None, None, None, None, None
+#         )
+#         self.sample_poi_dict = {
+#             "_id": "test",
+#             "address": "123 test dr.",
+#             "class": "EATERY",
+#             "hours_of_operation": {"Monday": "0"},
+#             "image_url": "https://test.com",
+#             "location": {"longitude": "0", "latitude": "0"},
+#             "name": "Test",
+#             "type": "queue",
 #         }
-#         self.poi_suggestion_id = "0"
 
-#     def test_get_all_poi(self):
-#         poi_obj_list = self.poi_service.get_all_POI()
-#         all_poi_db = [doc.to_dict() for doc in self.poi_ref.stream()]
-#         self.assertEqual([poi.to_dict() for poi in poi_obj_list], all_poi_db)
-
-#     def test_get_all_poi_exception(self):
-#         with self.assertRaises(Exception):
-#             poi_obj_list = self.poi_service.get_all_POI()
-#             all_poi_db = [doc.to_dict() for doc in self.poi_sug_ref.stream()]
-#             self.assertEqual([poi.to_dict() for poi in poi_obj_list], all_poi_db)
+#     def test_list_POI(self, firebase_mock):
+#         firebase_mock().document().get().to_dict = MagicMock(
+#             return_value=self.sample_poi_dict
+#         )
+#         self.assertEqual(self.sample_poi_dict, list_POI())
 
 #     def test_get_POI(self):
-#         poi_id = "tim_hortons_musc"
-#         poi = self.poi_service.get_POI(poi_id).to_dict()
-#         poi_db = self.poi_ref.document(poi_id).get().to_dict()
-#         self.assertEqual(poi, poi_db)
+#         pass
 
 #     def test_get_POI_POINotFoundError(self):
-#         with self.assertRaises(POINotFoundError):
-#             poi_id = "tim_hortons_bsb"
-#             poi = self.poi_service.get_POI(poi_id).to_dict()
-#             poi_db = self.poi_ref.document(poi_id).get().to_dict()
-#             self.assertEqual(poi, poi_db)
+#         pass
 
 #     # TODO: Update test as ID is no longer returned
 #     # Testing that poi suggestion has been added to the data base by checking matching id
@@ -62,6 +59,3 @@
 #     #             "submitted_by": "P-body and Atlas",
 #     #         }
 #     #         self.poi_service.suggest_new_POI(poi_suggestion_error)
-
-#     def tearDown(self):
-#         self.poi_sug_ref.document(self.poi_suggestion_id).delete()
