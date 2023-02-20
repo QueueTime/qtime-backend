a18fcd90752672185eed2bd775c6a9fb1429e8ba
Youssef Mikhail <youssef.mikhail@outlook.com>
2023-02-20 13:56:54 -0500

    Add comments to wait_time endpoint functions

--- app/wait_time/api.py
+++ app/wait_time/api.py
@@ -11,6 +11,16 @@ from app.common import BaseApiError
 def update_user_location(
     user: User, location_data: Dict[str, float], token_info: Dict[str, Any]
 ):
+    """
+    Takes in a JSON body with location data in the following format:
+    {
+        "latitude": float,
+        "longitude": float
+    }
+
+    Latitide is expected to be a float between -90 and 90. Longitude is expected to be
+    a float between -180 and 180
+    """
     try:
         user_location = UserLocation.from_dict(
             uid_to_aid(token_info["uid"]), location_data
@@ -27,6 +37,16 @@ def update_user_location(
 def submit_user_estimate(
     user: User, estimate_data: Dict[str, int], poi_id: str, **kwargs
 ):
+    """
+    Receives poi_id from the request path and estimate data from the request body.
+    Estimate data is expected to be in the following JSON format:
+    {
+        "wait_time_estimate": int
+    }
+
+    Wait time estimate is expected to be an int greater than or equal to 0.
+    poi_id is expected to be a valid POI ID.
+    """
     # TODO: implement user estimate submission
     # If we need more POI data, change this to fetch the actual POI object instead of just using poi_id
     try:
