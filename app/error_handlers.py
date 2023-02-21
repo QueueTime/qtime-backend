fd542ba8a3b9cf1d48befec0b129b0c0426c2661
Youssef Mikhail <youssef.mikhail@outlook.com>
2023-02-20 21:48:54 -0500

    Remove print statements

--- app/error_handlers.py
+++ app/error_handlers.py
@@ -4,10 +4,8 @@ from app.base_api_error import BaseApiError
 
 
 def handle_base_api_error(e: BaseApiError):
-    print("BaseApiError triggered")
     return e.build_error()
 
 
 def handle_generic_exception(e: Exception):
-    print("Generic exception triggered")
     return {"error": str(e)}, 500
