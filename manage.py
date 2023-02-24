0e25fcab3c77d36a5b5c94d84d61281f33890246
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-02-24 16:52:16 -0500

    Add code running the app in production mode

--- manage.py
+++ manage.py
@@ -1,4 +1,7 @@
+import os
+
 from setup import initialize_firebase, start_server, FIREBASE_CERT_PATH
+
 
 initialize_firebase(FIREBASE_CERT_PATH)
 app = start_server(__name__)
@@ -10,4 +13,13 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    env = os.environ.get("ENV", "dev")
+    if env == "prod":
+        # Run the app in production mode with waitress.
+        from waitress import serve
+
+        serve(app, host="0.0.0.0", port=5000)
+    elif env == "dev":
+        app.run(debug=True)
+    else:
+        raise Exception("Unsupported environment, must one of [dev, prod].")
