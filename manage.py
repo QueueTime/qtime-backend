03f4ed445ba29678efb1111ef51065aec790779f
Benjamin Kostiuk <benkostiuk1@gmail.com>
2023-03-07 18:21:36 -0500

    [FIX] Ensure log messages appear on a single line for easy filtering

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 import os
+import re
 from datetime import datetime, timezone
 from flask import request
 from setup import initialize_firebase, start_server, FIREBASE_CERT_PATH
@@ -22,15 +23,16 @@ if __name__ == "__main__":
 
         logger = logging.getLogger("waitress")
         logger.setLevel(logging.DEBUG)
-        NEWLINE = "\n"
+        SEP = " "
+        REPLACE_EXPR = "\s*[\n\r\t]\s*"
 
         @app.app.after_request
         def after_request_func(response):
             logger.debug(
                 f'[{datetime.now(timezone.utc).strftime("%d/%b/%Y %H:%M:%S")}] {request.remote_addr} "{request.method} {request.path}{"?"+request.query_string.decode("utf-8") if request.query_string else ""} {request.scheme.upper()}" {response.status_code}'
-                f'{NEWLINE + str(request.form.to_dict()) if request.form else ""}'
-                f'{NEWLINE + str(request.json) if request.is_json else ""}'
-                f'{NEWLINE + str(response.data.decode("utf-8")).strip() if response.data else ""}'
+                f'{SEP + str(request.form.to_dict()) if request.form else ""}'
+                f'{SEP + str(request.json) if request.is_json else ""}'
+                f'{SEP + re.sub(REPLACE_EXPR, SEP, response.data.decode("utf-8")) if response.data else ""}'
             )
             return response
 
