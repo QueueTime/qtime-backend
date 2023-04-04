42132e6c6e659bed7443c86c4b269e1dc2627778
Youssef Mikhail <youssef.mikhail@outlook.com>
2023-04-04 01:34:24 -0400

    Fix SimpleMap

--- app/common.py
+++ app/common.py
@@ -21,3 +21,7 @@ class SimpleMap:
 
     def to_dict(self) -> Dict[str, Any]:
         return self.__dict__
+
+    @staticmethod
+    def from_dict(dict: Dict[str, Any]):
+        return SimpleMap(**dict)
